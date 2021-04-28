08ee2825fa557b881424c260f70d9073388b57c1
aneek <aneek.khan@outlook.com>
2021-04-28 16:18:39 +0600

    Worked till total deaths

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="text-center bg-green-500 p-4 mb-10 text-white shadow-lg">
+      <div class="text-3xl md:text-5xl font-bold mb-3">
+          <i class="fa fa-viruses"></i> Covid-19 Tracker</div>
+  </header>
+</template>
+
+<script>
+export default {
+    name:'Header'
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<main v-if="!loading" >
+  <DataTitle :text="title" :dateData="dateData"/>
+  <DataBoxes :stats="stats" class="container"/>
+</main>
+<main class="flex flex-col align-center justify-center text-center" v-else>
+  <div class="text-gray-500 text-3xl mt-10 mb-6">
+    Fetching Data
   </div>
+  <img :src="loadingImage" class="w-24 m-auto" alt="">
+
+</main>
+
 </template>
 
 <script>
+
+import DataTitle from '../components/DataTitle.vue'
+import DataBoxes from '../components/DataBoxes.vue'
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
+  data(){
+    return{
+      loading: true,
+      title: 'Global',
+      dateData:'',
+      stats: {},
+      countries:[],
+      loadingImage: require('../assets/hourglass.gif')
+
+    }
+  },
+  methods:{
+    async fetchCovidData(){
+      const response = await fetch('https://api.covid19api.com/summary')
+      const data = await response.json()
+      return data
+
+    }
+  },
   components: {
-    HelloWorld
+    DataTitle,
+    DataBoxes
+    
+  },
+  async created(){
+    const data = await this.fetchCovidData()
+    this.dateData = data.Date
+    this.stats = data.Global
+    this.countries = data.countries
+    this.loading = false
   }
-}
+  
+    } 
+     
 </script>
+
+<style>
+.container{
+  display: inline-block;
+  justify-content: center;
+  padding: 2rem;
+}
+</style>
